8db7fac7cb075ae0a4f1482975dbdb1a2a78aac8
jedidi jedidi <jedidi.jedidi6@gmail.com>
2016-07-30 15:24:29 +0100

    I Added the unity Project

---
+++ Testing Windows Form On Unity/Assets/MyWindowsFormPlugin/WindowsFormManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Windows.Forms;
+using System.Drawing;
+
+public class WindowsFormManager {
+
+	/// <summary>
+	/// Creates a new form.
+	/// </summary>
+	/// <returns>The form that we created.</returns>
+	/// <param name="name">the form name .</param>
+	/// <param name="text">the form text.</param>
+	public Form CreateForm(string name="form",string text="form"){
+		Form form=new Form();
+		form.Name=name;
+		form.Text = text;
+
+		return form;
+	}
+
+	/// <summary>
+	/// Shows the passed form.
+	/// </summary>
+	/// <param name="form">The form that you want to show.</param>
+	public void ShowForm(Form form){
+		form.ShowDialog ();
+	}
+
+	public void UpdateForm(Form form){
+		form.Update ();
+	}
+
+	/// <summary>
+	/// Creates a button.
+	/// </summary>
+	/// <returns>The button that we create.</returns>
+	/// <param name="form">the Form that we want to add the button into.</param>
+	/// <param name="name">the name of the button.</param>
+	/// <param name="text">the text of the button.</param>
+	/// <param name="posX">the X position of the button</param>
+	/// <param name="posY">the Y position of the button.</param>
+	/// <param name="width">the width of the button.</param>
+	/// <param name="height">the height of the button.</param>
+	public Button CreateButton(Form form,string name="button",string text="button",int posX=50,int posY=50,int width =80,int height=30){
+		Button button=new Button();
+		button.Name = name;
+		button.Text = text;
+		button.Location = new Point(posX,posY);
+		button.Width = width;
+		button.Height = height;
+		form.Controls.Add (button);
+		return button;
+	}		
+
+	/// <summary>
+	/// Creates a textBox.
+	/// </summary>
+	/// <returns>The textBox that we create.</returns>
+	/// <param name="form">the Form that we want to add the textBox into.</param>
+	/// <param name="name">the name of the textBox.</param>
+	/// <param name="text">the text of the textBox.</param>
+	/// <param name="posX">the X position of the textBox</param>
+	/// <param name="posY">the Y position of the textBox.</param>
+	/// <param name="width">the width of the textBox.</param>
+	/// <param name="height">the height of the textBox.</param>
+	public TextBox CreateTextBox(Form form,string name="textBox",string text="",int posX=50,int posY=50,int width =120,int height=30){
+		TextBox textBox =new TextBox();
+		textBox.Name = name;
+		textBox.Text = text;
+		textBox.Location = new Point(posX,posY);
+		textBox.Width = width;
+		textBox.Height = height;
+		form.Controls.Add (textBox);
+		return textBox;
+	}
+}
---
+++ Testing Windows Form On Unity/Assets/WindowsFrom.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Windows.Forms;
+
+public class WindowsFrom : MonoBehaviour {
+
+	WindowsFormManager wfm=new WindowsFormManager();
+
+	Form form;
+	Button button1;
+	TextBox textBox1;
+
+	void Update () {
+		if( form !=null){
+			wfm.UpdateForm (form);
+		}
+			
+		if(Input.GetKeyDown(KeyCode.Space)){
+			CreateDebugForm ();
+		}
+	}
+
+	void CreateDebugForm(){
+		form = wfm.CreateForm ("form","Debug Form");
+		button1 = wfm.CreateButton (form,"button1","Debug",50,50,80,30);
+		textBox1 = wfm.CreateTextBox (form,"textBox1","",50,80,120,30);
+
+		button1.Click += Button1_Click;
+
+		wfm.ShowForm (form);
+	}
+
+	void Button1_Click (object sender, System.EventArgs e)
+	{
+		Debug.Log(textBox1.Text);
+	}
+}
